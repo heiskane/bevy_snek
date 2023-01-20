493bf024d16ca3704d88920f692dbc28f2f87874
heiskane <henipe@gmail.com>
2023-01-20 20:23:23 +0200

    spawn snack and validate directions properly

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use bevy::prelude::*;
+use rand::{thread_rng, Rng};
 
-// TODO: Global snek size
+const SNEK_SIZE: f32 = 25.0;
 
-// TODO: Make direction request to avoit fast movement to go wrong way
 #[derive(Component, Debug, Eq, PartialEq)]
 enum Direction {
     Up,
@@ -15,7 +15,10 @@ enum Direction {
 struct SnekBlock(i32); // Custom type for this? To share it
 
 #[derive(Component, Debug)]
-struct Snek(i32);
+struct Snek {
+    length: i32,
+    direction: Direction,
+}
 
 #[derive(Resource)]
 struct MoveTimer(Timer);
@@ -27,20 +30,27 @@ fn setup(mut commands: Commands) {
 fn create_snek(mut commands: Commands) {
     let snek_sprite = SpriteBundle {
         sprite: Sprite {
-            color: Color::rgb(0.25, 0.25, 0.25),
-            custom_size: Some(Vec2::new(25.0, 25.0)),
+            color: Color::rgb(0.05, 0.05, 0.05),
+            custom_size: Some(Vec2::new(SNEK_SIZE, SNEK_SIZE)),
             ..default()
         },
         ..default()
     };
-    commands.spawn((Snek(3), Direction::Left, snek_sprite));
+    commands.spawn((
+        Snek {
+            length: 3,
+            direction: Direction::Left,
+        },
+        Direction::Left,
+        snek_sprite,
+    ));
 }
 
 fn move_snek(
     mut commands: Commands,
     time: Res<Time>,
     mut timer: ResMut<MoveTimer>,
-    mut snek_query: Query<(&Snek, &Direction, &mut Transform)>,
+    mut snek_query: Query<(&mut Snek, &Direction, &mut Transform)>,
     mut snek_block_query: Query<(Entity, &mut SnekBlock)>,
 ) {
     if !timer.0.tick(time.delta()).just_finished() {
@@ -48,34 +58,47 @@ fn move_snek(
     }
 
     for (entity, mut snek_block) in snek_block_query.iter_mut() {
-        print!("{snek_block:?}");
-        if snek_block.0 == 0 {
+        if snek_block.0 == 1 {
             commands.entity(entity).despawn();
         } else {
             snek_block.0 -= 1;
         }
     }
 
-    for (snek, dir, mut transform) in snek_query.iter_mut() {
+    for (mut snek, dir, mut transform) in snek_query.iter_mut() {
         commands.spawn((
             SpriteBundle {
                 sprite: Sprite {
                     color: Color::BLACK,
-                    custom_size: Some(Vec2::new(25.0, 25.0)),
+                    custom_size: Some(Vec2::new(SNEK_SIZE, SNEK_SIZE)),
                     ..default()
                 },
                 transform: transform.clone(),
                 ..default()
             },
-            SnekBlock(snek.0),
+            SnekBlock(snek.length),
         ));
 
         println!("{snek:?} - {dir:?}");
-        match dir {
-            Direction::Left => transform.translation.x -= 25.0,
-            Direction::Right => transform.translation.x += 25.0,
-            Direction::Up => transform.translation.y += 25.0,
-            Direction::Down => transform.translation.y -= 25.0,
+
+        if *dir == Direction::Left && snek.direction != Direction::Right {
+            snek.direction = Direction::Left;
+        }
+        if *dir == Direction::Right && snek.direction != Direction::Left {
+            snek.direction = Direction::Right;
+        }
+        if *dir == Direction::Up && snek.direction != Direction::Down {
+            snek.direction = Direction::Up;
+        }
+        if *dir == Direction::Down && snek.direction != Direction::Up {
+            snek.direction = Direction::Down;
+        }
+
+        match snek.direction {
+            Direction::Left => transform.translation.x -= SNEK_SIZE,
+            Direction::Right => transform.translation.x += SNEK_SIZE,
+            Direction::Up => transform.translation.y += SNEK_SIZE,
+            Direction::Down => transform.translation.y -= SNEK_SIZE,
         };
     }
 }
@@ -84,28 +107,49 @@ fn snek_controls(
     key_input: Res<Input<KeyCode>>,
     mut snek_dir_query: Query<&mut Direction, With<Snek>>,
 ) {
+    // TODO: implement queue fot this not to cancel out fast keystrokes?
     for mut snek_dir in snek_dir_query.iter_mut() {
-        if key_input.just_pressed(KeyCode::Up) && *snek_dir != Direction::Down {
+        if key_input.just_pressed(KeyCode::Up) {
             *snek_dir = Direction::Up
         }
-        if key_input.just_pressed(KeyCode::Right) && *snek_dir != Direction::Left {
+        if key_input.just_pressed(KeyCode::Right) {
             *snek_dir = Direction::Right
         }
-        if key_input.just_pressed(KeyCode::Left) && *snek_dir != Direction::Right {
+        if key_input.just_pressed(KeyCode::Left) {
             *snek_dir = Direction::Left
         }
-        if key_input.just_pressed(KeyCode::Down) && *snek_dir != Direction::Up {
+        if key_input.just_pressed(KeyCode::Down) {
             *snek_dir = Direction::Down
         }
     }
 }
 
+fn generate_snacks(mut commands: Commands) {
+    let mut rng = thread_rng();
+    let x: f32 = rng.gen_range(-250.0..250.0);
+    let y: f32 = rng.gen_range(-250.0..250.0);
+    commands.spawn(SpriteBundle {
+        sprite: Sprite {
+            color: Color::RED,
+            custom_size: Some(Vec2::new(SNEK_SIZE, SNEK_SIZE)),
+            ..default()
+        },
+        transform: Transform::from_translation(Vec3::new(
+            x - (x % SNEK_SIZE),
+            y - (y % SNEK_SIZE),
+            0.0,
+        )),
+        ..default()
+    });
+}
+
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
-        .insert_resource(MoveTimer(Timer::from_seconds(0.5, TimerMode::Repeating)))
+        .insert_resource(MoveTimer(Timer::from_seconds(0.15, TimerMode::Repeating)))
         .add_startup_system(setup)
         .add_startup_system(create_snek)
+        .add_startup_system(generate_snacks)
         .add_system(snek_controls)
         .add_system(move_snek)
         .run();
