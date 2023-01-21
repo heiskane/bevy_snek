061c6fcd3213d4760b7bd209fc08fdd8ea6410ba
heiskane <henipe@gmail.com>
2023-01-21 11:13:11 +0200

    working snacks

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use bevy::prelude::*;
+use bevy::{prelude::*, sprite::collide_aabb::collide};
 use rand::{thread_rng, Rng};
 
-const SNEK_SIZE: f32 = 25.0;
+const SNEK_SIZE: f32 = 15.0;
 
 #[derive(Component, Debug, Eq, PartialEq)]
 enum Direction {
@@ -10,6 +10,9 @@ enum Direction {
     Left,
     Right,
 }
+
+#[derive(Component)]
+struct Snack;
 
 #[derive(Component, Debug)]
 struct SnekBlock(i32); // Custom type for this? To share it
@@ -46,7 +49,7 @@ fn create_snek(mut commands: Commands) {
     ));
 }
 
-fn move_snek(
+fn snek_movement(
     mut commands: Commands,
     time: Res<Time>,
     mut timer: ResMut<MoveTimer>,
@@ -107,7 +110,7 @@ fn snek_controls(
     key_input: Res<Input<KeyCode>>,
     mut snek_dir_query: Query<&mut Direction, With<Snek>>,
 ) {
-    // TODO: implement queue fot this not to cancel out fast keystrokes?
+    // TODO: move immidiately on keypress
     for mut snek_dir in snek_dir_query.iter_mut() {
         if key_input.just_pressed(KeyCode::Up) {
             *snek_dir = Direction::Up
@@ -124,23 +127,54 @@ fn snek_controls(
     }
 }
 
-fn generate_snacks(mut commands: Commands) {
+fn generate_snacks(mut commands: Commands, snack_query: Query<&Snack>) {
+    if !snack_query.is_empty() {
+        return;
+    }
+
     let mut rng = thread_rng();
     let x: f32 = rng.gen_range(-250.0..250.0);
     let y: f32 = rng.gen_range(-250.0..250.0);
-    commands.spawn(SpriteBundle {
-        sprite: Sprite {
-            color: Color::RED,
-            custom_size: Some(Vec2::new(SNEK_SIZE, SNEK_SIZE)),
+    commands.spawn((
+        SpriteBundle {
+            sprite: Sprite {
+                color: Color::RED,
+                custom_size: Some(Vec2::new(SNEK_SIZE, SNEK_SIZE)),
+                ..default()
+            },
+            transform: Transform::from_translation(Vec3::new(
+                x - (x % SNEK_SIZE),
+                y - (y % SNEK_SIZE),
+                0.0,
+            )),
             ..default()
         },
-        transform: Transform::from_translation(Vec3::new(
-            x - (x % SNEK_SIZE),
-            y - (y % SNEK_SIZE),
-            0.0,
-        )),
-        ..default()
-    });
+        Snack,
+    ));
+}
+
+fn eat_snacks(
+    mut commands: Commands,
+    mut snek_block_query: Query<&mut SnekBlock>,
+    mut snek_query: Query<(&mut Snek, &Transform)>,
+    snack_query: Query<(Entity, &Transform), With<Snack>>,
+) {
+    for (mut snek, snek_loc) in snek_query.iter_mut() {
+        for (entity, snack) in snack_query.iter() {
+            if let Some(_) = collide(
+                snek_loc.translation,
+                Vec2::new(snek_loc.scale.x, snek_loc.scale.y),
+                snack.translation,
+                Vec2::new(snack.scale.x, snack.scale.y),
+            ) {
+                commands.entity(entity).despawn();
+                snek.length += 1;
+                snek_block_query.for_each_mut(|mut block| {
+                    block.0 += 1;
+                });
+            }
+        }
+    }
 }
 
 fn main() {
@@ -151,6 +185,8 @@ fn main() {
         .add_startup_system(create_snek)
         .add_startup_system(generate_snacks)
         .add_system(snek_controls)
-        .add_system(move_snek)
+        .add_system(snek_movement)
+        .add_system(generate_snacks)
+        .add_system(eat_snacks)
         .run();
 }
